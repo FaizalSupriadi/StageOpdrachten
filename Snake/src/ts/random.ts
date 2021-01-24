d679a77ae0d57e197179e2ffcdf63af60619616e
FaizalSupriadi <faizal.faizalrachmansupriadi@student.hu.nl>
2021-01-24 22:28:35 +0100

    Added random

---
+++ Snake/src/ts/random.ts
@@ -0,0 +1,9 @@
+// To be used for random food placement
+export class Random {
+    static Generate(min: number, max: number): number {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+}
